ad301ee3fb83ce370490f3ae562c85735852b2b6
Pablo Hidalgo Martínez <thefranhi@hotmail.com>
2017-12-02 16:04:07 +0100

    Documentation Added

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import java.io.IOException;
 
 import com.jfoenix.controls.JFXButton;
-
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +10,16 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/**
+ * 
+ * @author Pablo Hidalgo Martínez
+ * @version 1.0.0
+ * @see Controller
+ * @see MainLayout
+ * Star Wars related simple App
+ * @link http://www.jfoenix.com/documentation.html
+ * @link https://github.com/franhida/SchoolJavaFX
+ */
 public class Main extends Application {
 	private Stage stage;
 
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,25 +3,40 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 
+/**
+ * 
+ * @author Pablo Hidalgo Martínez
+ * @see Main
+ * @see MainLayout
+ *
+ */
 public class Controller {
 
 	@FXML
 	private ImageView ivRedSymbol, ivBlackSymbol;
 
+	/**
+	 * Method to set visible the Red Mandalorian symbol and hide the Black one so
+	 * both pictures are not showed at the same time. This method is used at
+	 * MainLayout : "btnRedSymbol" button.
+	 * 
+	 */
 	@FXML
 	public void showRedSymbol() {
 		ivBlackSymbol.setVisible(false);
 		ivRedSymbol.setVisible(true);
 	}
 
+	/**
+	 * Method to set visible the Black Mandalorian symbol and hide the Red one so
+	 * both pictures are not showed at the same time.This method is used at
+	 * MainLayout : "btnBlackSymbol" button.
+	 * 
+	 */
 	@FXML
 	public void showBlackSymbol() {
 		ivRedSymbol.setVisible(false);
 		ivBlackSymbol.setVisible(true);
 	}
 
-	@FXML
-	private void initialize() {
-
-	}
 }
